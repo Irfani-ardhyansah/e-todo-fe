85384f13e3461e1eb8f254ad1adc165705deb314
Irfani Ardhyansah <irfaniardhyansah@MacBook-Air-Irfani.local>
2024-08-29 21:49:18 +0700

    add button to active in sidebar

--- src/components/home/Sidebar.js
+++ src/components/home/Sidebar.js
@@ -1,6 +1,30 @@
+import React, { useState } from 'react';
+
 const Sidebar = () => {
-    const items = Array.from({ length: 11 }, (_, index) => index + 1);
+    // const items = Array.from({ length: 11 }, (_, index) => index + 1);
+
+    const items = [
+        { id: 1, name: 'Item 1' }
+      ];
+
+      let newItemIndex = 4;
+
+      while (items.length < 15) {
+        const newItem = { id: newItemIndex, name: `Item ${newItemIndex}` };
+        items.push(newItem);
+        newItemIndex++;
+      }
+
+    const [isActiveId, setActiveId] = useState(null)
     
+    const toggleClass = (id) => {
+        if(isActiveId == id) {
+            setActiveId(null)
+        } else {
+            setActiveId(id);
+        }
+    }
+
     return (
         <>
             <div className="col-2">
@@ -9,14 +33,18 @@ const Sidebar = () => {
                         <button className="btn-filter-sidebar" > Created </button>
                     </div>
                     <ul className="list-group list-group-flush">
-                        {items.map((item) => (
-                            <li className="list-group-item">
+                        {items.map((item, index) => (
+                            <li 
+                                className={`list-group-item ${isActiveId === item.id ? 'active' : ''}`}
+                                key={item.id}
+                                onClick={() => toggleClass(item.id)}
+                                >
                                 <div className="content-top mb-2">
-                                    CRM on Dev is Broken
+                                    {item.name}
                                 </div>
                                 <div className="content-bottom d-flex justify-content-between mb-2">
                                     <div className="left-side">
-                                        RAS-1093
+                                        RAS-{item.id}
                                     </div>
                                     <div className="right-side">
                                         IM
